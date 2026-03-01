337bbb5e79dbc5037d3ec6dc3eea943e28126987
{RichardPDEV} <{richardperez079@gmail.com}>
2026-03-01 13:44:23 -0400

    Se creo y se configuro PostgeSQL con Docker y Flyway

---
+++ src/main/java/com/richardpdev/inventoryerp/InventoryErpApplication.java
@@ -0,0 +1,13 @@
+package com.richardpdev.inventoryerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richardpdev/inventory/InventoryErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richardpdev.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
